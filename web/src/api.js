77fd7322334ed20bbdfb2971724cb4f201896d4c
Edgar Egidio <37923860+edgaregidio@users.noreply.github.com>
2022-05-23 19:49:12 -0300

    att api

--- web/src/api.js
+++ web/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/'
+  baseURL: 'https://varzeabrasil-app-production.up.railway.app'
 })
 
 export default api
